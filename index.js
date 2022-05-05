4b70428560262935335aa1907b0ffdd304358804
not-the <87151784+not-the@users.noreply.github.com>
2022-05-05 02:22:56 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+trackMouse();
+
+
+const elContextMenu = dom('context_menu');
+function contextMenu(event) {
+    event.preventDefault();
+
+    let y = mouseY;
+    let x = mouseX;
+    let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    if(mouseX + 240 >= vw) {
+        // x -= 240;
+        x = vw - 240;
+    }
+    elContextMenu.style.top = `${y}px`;
+    elContextMenu.style.left = `${x}px`;
+    elContextMenu.focus();
+}
